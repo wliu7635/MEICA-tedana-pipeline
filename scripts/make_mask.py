78daa82ff9f7c0338c7bb1e0e54fca6ebfd77f8b
wliu7635 <liu03862@umn.edu>
2026-03-04 09:46:15 -0600

    Create make_mask.py

---
+++ scripts/make_mask.py
@@ -0,0 +1,15 @@
+ import os
+ import nibabel as nib
+ from nilearn.masking import compute_epi_mask  # see official API  [3](https://blog.csdn.net/weixin_45090728/article/details/114285892)
+ 
+ # ① set your Echo‑1 NIfTI path
+ epi_path = r"D:\MEICA\raw\sub-01\sub-01_task-rest_echo-1_bold.nii.gz"
+ 
+ # ② set your desired output path for the mask (directories will be created)
+ out_path = r"D:\MEICA\MEICA-derivatives\masks\sub-01\sub-01_task-rest_mask.nii.gz"
+ 
+ os.makedirs(os.path.dirname(out_path), exist_ok=True)
+ img = nib.load(epi_path)
+ mask_img = compute_epi_mask(img)
+ mask_img.to_filename(out_path)
+ print("Saved mask:", out_path)
